f09279db57930affbf02f50d5d7ef7c9384776e9
ffulauh <shangreed@msn.com>
2017-05-05 10:53:45 +0800

    测试trycatch

--- src/main/java/me/ffulauh/javalang/ExceptionUtils.java
+++ src/main/java/me/ffulauh/javalang/ExceptionUtils.java
@@ -4,14 +4,16 @@ package me.ffulauh.javalang;
  * Created by ffulauh on 2017/4/25.
  */
 public class ExceptionUtils {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         try{
-            String han="2,2,2";
-            String[] hans=han.split(",");
-            String haha=hans[5];
-        } catch (Exception e){
-            System.out.println(e.getMessage());
+            hehe();
+        }catch (Exception e){
+            e.printStackTrace();
         }
+    }
 
+    public static String hehe() throws Exception {
+        throw new Exception();
     }
 }
+
