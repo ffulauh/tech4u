1ed75cbeae3db3c3a50add3b6f34ee3d2cd72c48
ffulauh <shangreed@msn.com>
2017-05-22 14:30:28 +0800

    添加LambdaDemo类

---
+++ src/main/java/me/ffulauh/javalang/LambdaDemo.java
@@ -0,0 +1,14 @@
+package me.ffulauh.javalang;
+
+/**
+ * Created by ffulauh on 2017/5/17.
+ */
+public class LambdaDemo {
+    public static void main(String[] args) {
+
+    }
+
+    void printSomething(String msg){
+        System.out.println(msg);
+    }
+}
